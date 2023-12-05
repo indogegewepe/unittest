9fdce502b7a16ca80f499e592fd6b05e652e6095
Sedenter <64572332+indogegewepe@users.noreply.github.com>
2023-12-05 08:31:59 +0700

    p

--- GeometryCalculatorTest.py
+++ GeometryCalculatorTest.py
@@ -7,12 +7,36 @@ class GeometryCalculatorTest(unittest.TestCase):
     def setUp(self):
         self.alas = 5
         self.tinggi = 10
+        self.panjang = 5
+        self.lebar = 10
+        self.sisi = 5
 
     def test_luas_segitiga(self):
         #act
         actualResult = GeometryCalculator.luas_segitiga(self.alas, self.tinggi)
         #assert
         expectedResult = 0.5 * self.alas * self.tinggi
+        self.assertEqual(actualResult, expectedResult)
+
+    def test_luas_persegi_panjang(self):
+        #act
+        actualResult = GeometryCalculator.luas_persegi_panjang(self.panjang, self.lebar)
+        #assert
+        expectedResult = self.panjang * self.lebar
+        self.assertEqual(actualResult, expectedResult)
+
+    def test_luas_jajargenjang(self):
+        #act
+        actualResult = GeometryCalculator.luas_jajargenjang(self.alas, self.tinggi)
+        #assert
+        expectedResult = self.alas * self.tinggi
+        self.assertEqual(actualResult, expectedResult)
+
+    def test_luas_persegi(self):
+        #act
+        actualResult = GeometryCalculator.luas_persegi(self.sisi)
+        #assert
+        expectedResult = self.sisi * self.sisi
         self.assertEqual(actualResult, expectedResult)
 
 unittest.main()
--- geometry_calculator.py
+++ geometry_calculator.py
@@ -1,5 +1,14 @@
 class GeometryCalculator:
     def luas_segitiga(alas, tinggi):
-        return 1 * alas * tinggi
+        return 0.5 * alas * tinggi
+    def luas_persegi_panjang(panjang, lebar):
+        return panjang * lebar
+    def luas_jajargenjang(alas, tinggi):
+        return alas * tinggi
+    def luas_persegi(sisi):
+        return sisi * sisi
 
 print(GeometryCalculator.luas_segitiga(10, 5))
+print(GeometryCalculator.luas_persegi_panjang(10, 5))
+print(GeometryCalculator.luas_jajargenjang(10, 5))
+print(GeometryCalculator.luas_persegi(10))
