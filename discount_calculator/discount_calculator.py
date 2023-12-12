f148ef573591ac59637b58ae03a64e5c72eadbcc
Sedenter <64572332+indogegewepe@users.noreply.github.com>
2023-12-12 07:40:41 +0700

    cuk

--- discount_calculator/discount_calculator.py
+++ discount_calculator/discount_calculator.py
@@ -4,7 +4,7 @@ class DiscountCalculator:
     def calculate_discount(volume):
         if volume >= 1000:
             return 0.1
-        if volume >= 500:
+        elif volume >= 500:
             return 0.05
         else:
             return 0.0
--- discount_calculator/test_discount_calculator.py
+++ discount_calculator/test_discount_calculator.py
@@ -4,24 +4,20 @@ from discount_calculator import DiscountCalculator
 class DiscountCalculatorTest(unittest.TestCase):
 
     def setUp(self):
-        self.val1 = 1000
-        self.val05 = 500
-        self.val0 = 1
+        self.val1 = 1000129308109283
+        self.val05 = 999
+        self.val0 = 499
 
     def test_discount_calculator1(self):
-        actual_discount = DiscountCalculator.calculate_discount(self.val1)
-        expected_discount = 0.1
-        self.assertEqual(actual_discount, expected_discount)
+        self.assertEqual(DiscountCalculator.calculate_discount(self.val1), 0.1, "Failed for volume 1000")
 
     def test_discount_calculator05(self):
-        actual_discount = DiscountCalculator.calculate_discount(self.val05)
-        expected_discount = 0.05
-        self.assertEqual(actual_discount, expected_discount)
+        self.assertEqual(DiscountCalculator.calculate_discount(self.val05), 0.05, "Failed for volume 500")
 
     def test_discount_calculator0(self):
         actual_discount = DiscountCalculator.calculate_discount(self.val0)
         expected_discount = 0.0
-        self.assertEqual(actual_discount, expected_discount)
+        self.assertEqual(actual_discount, expected_discount, "Failed for volume 0")
 
 if __name__=='__main__':
     unittest.main()
