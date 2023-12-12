dfe5e7447e55e958d776316e1b012d1b9b8babbf
Sedenter <64572332+indogegewepe@users.noreply.github.com>
2023-12-12 08:05:53 +0700

    up

--- discount_calculator/discount_calculator.py
+++ discount_calculator/discount_calculator.py
@@ -1,14 +1,16 @@
 #discount_calculator.py
 
-class DiscountCalculator:
-    def calculate_discount(volume):
-        if volume >= 1000:
-            return 0.1
-        elif volume >= 500:
-            return 0.05
-        else:
-            return 0.0
-        
-print(DiscountCalculator.calculate_discount(1000))
-print(DiscountCalculator.calculate_discount(500))
-print(DiscountCalculator.calculate_discount(1))
+def calculate_discount(volume):
+    if volume >= 1000:
+        return 0.1
+    elif volume >= 500:
+        return 0.05
+    else:
+        return 0.0
+
+for i in range(0, 1500):
+    print(calculate_discount(i))
+
+# print(calculate_discount(1000))
+# print(calculate_discount(500))
+# print(calculate_discount(1))
--- discount_calculator/test_discount_calculator.py
+++ discount_calculator/test_discount_calculator.py
@@ -1,7 +1,19 @@
 import unittest
-from discount_calculator import DiscountCalculator
+from discount_calculator import calculate_discount
 
 class DiscountCalculatorTest(unittest.TestCase):
+
+    def test_calculate_discount_input(self):
+        for i in range(0, 499):
+            self.assertEqual(calculate_discount(i), 0.0, "Failed for volume 0")
+        for i in range(500, 999):
+            self.assertEqual(calculate_discount(i), 0.05, "Failed for volume 500")
+        for i in range(1000, 1500):
+            self.assertEqual(calculate_discount(i), 0.1, "Failed for volume 1000")
+
+if __name__=='__main__':
+    unittest.main()
+
 
     # def setUp(self):
     #     self.val = 499
@@ -16,14 +28,3 @@ class DiscountCalculatorTest(unittest.TestCase):
     #     actual_discount = DiscountCalculator.calculate_discount(self.val)
     #     expected_discount = 0.0
     #     self.assertEqual(actual_discount, expected_discount, "Failed for volume 0")
-
-    def test_calculate_discount_zero_input(self):
-        for i in range(0, 499):
-            self.assertEqual(DiscountCalculator.calculate_discount(i), 0.0, "Failed for volume 0")
-        for i in range(500, 999):
-            self.assertEqual(DiscountCalculator.calculate_discount(i), 0.05, "Failed for volume 500")
-        for i in range(1000, 1200):
-            self.assertEqual(DiscountCalculator.calculate_discount(i), 0.1, "Failed for volume 1000")
-
-if __name__=='__main__':
-    unittest.main()
