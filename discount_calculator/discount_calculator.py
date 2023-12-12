55a13fb59a166184192c7e2e4a0aa54b5bf802ef
Sedenter <64572332+indogegewepe@users.noreply.github.com>
2023-12-12 07:27:48 +0700

    ah

--- discount_calculator/discount_calculator.py
+++ discount_calculator/discount_calculator.py
@@ -1,9 +1,12 @@
 #discount_calculator.py
 
-def calculate_discount(volume):
-    if volume >= 1000:
-        return 0.1
-    if volume >= 500:
-        return 0.05
-    else:
-        return 0.0
+class DiscountCalculator:
+    def calculate_discount(volume):
+        if volume >= 1000:
+            return 0.1
+        if volume >= 500:
+            return 0.05
+        else:
+            return 0.0
+        
+print(DiscountCalculator.calculate_discount(1))
--- discount_calculator/test_discount_calculator.py
+++ discount_calculator/test_discount_calculator.py
@@ -1,4 +1,26 @@
 import unittest
-from discount_calculator import calculate_discount
+from discount_calculator import DiscountCalculator
 
-class GeometryCalculatorTest(unittest.TestCase):
+class DiscountCalculatorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.val1 = 1000
+        self.val05 = 500
+        self.val0 = 1
+
+    def test_discount_calculator1(self):
+        actual_discount = DiscountCalculator.calculate_discount(self.val1)
+        expected_discount = 0.1
+        self.assertEqual(actual_discount, expected_discount)
+
+    def test_discount_calculator05(self):
+        actual_discount = DiscountCalculator.calculate_discount(self.val05)
+        expected_discount = 0.05
+        self.assertEqual(actual_discount, expected_discount)
+
+    def test_discount_calculator0(self):
+        actual_discount = DiscountCalculator.calculate_discount(self.val0)
+        expected_discount = 0.0
+        self.assertEqual(actual_discount, expected_discount)
+
+unittest.main()
